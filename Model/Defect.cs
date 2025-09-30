0e743978a5b585c857352832887e692655f714e0
somalang <sohyun@ati2000.co.kr>
2025-09-30 17:16:16 +0900

    feat : add defect model and example UI

---
+++ Model/Defect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlarfApplication.Model
+{
+    internal class Defect
+    {
+        // 아이디, x좌표, y좌표, 인덱스, row, col 등이 들어감
+    }
+}
