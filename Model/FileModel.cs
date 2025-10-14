9a87e5536eb94b83e37222ed184c281353149d2b
somalang <sohyun@ati2000.co.kr>
2025-10-14 17:18:25 +0900

    feat : add fileModel, ModelBase, RelayCommand

--- Model/FileModel.cs
+++ Model/FileModel.cs
@@ -8,10 +8,9 @@ namespace KlarfApplication.Model
 {
     public class FileModel : ModelBase
     {
-        // 파일인지 디렉터리인지 확인할 수 있는 구조
         public readonly bool _isFile;
         public readonly string _fileName;
-        public readonly string _fileExtension { get; }
+        public readonly string _fileExtension;
 
     }
 }
--- Model/ModelBase.cs
+++ Model/ModelBase.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace KlarfApplication.Model
 {
+    // 모델이 직접 뷰에 바인딩되어 동적으로 UI 갱신하는 경우는 모델베이스에 넣기
     public class ModelBase
     {
         public event PropertyChangedEventHandler PropertyChanged;
